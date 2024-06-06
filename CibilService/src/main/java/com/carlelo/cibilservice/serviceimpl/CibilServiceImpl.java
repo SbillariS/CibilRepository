136687ef7a4e79bb9de46820541a1710c2ba4fb6
SbillariS <shitalbillari8@gmail.com>
2024-06-07 00:55:36 +0530

    add fromdate and todate method by shital

--- CibilService/src/main/java/com/carlelo/cibilservice/serviceimpl/CibilServiceImpl.java
+++ CibilService/src/main/java/com/carlelo/cibilservice/serviceimpl/CibilServiceImpl.java
@@ -1,5 +1,9 @@
 package com.carlelo.cibilservice.serviceimpl;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Random;
 
 import org.springframework.stereotype.Service;
@@ -52,8 +56,29 @@ public class CibilServiceImpl implements CibilServiceI
 	  	  details.setCibilScore(score);
 	  	  details.setRemark(getRemarkForScore(score));
 	  	  details.setIsApplicable(applicable(score));
-	  	  details.setFromDate("02-06-2024");
-	  	  details.setToDate("16-06-2024");
+	  	  details.setFromDate(formdate());
+	  	  details.setToDate(todate());
 		  return details;  
+	}
+	
+	public static String formdate()
+	{
+		Date date=new Date();
+       SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");  
+	   String beforedate= sdf.format(date);
+	   return beforedate; 
+	}
+	public String todate()
+	{
+	    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		Calendar cal = Calendar.getInstance();  
+        try{  
+           cal.setTime(sdf.parse(CibilServiceImpl.formdate()));  
+        }catch(ParseException e){  
+            e.printStackTrace();  
+         }       
+        cal.add(Calendar.DAY_OF_MONTH, 14);  
+        String dateAfter = sdf.format(cal.getTime());  
+        return dateAfter;
 	}
 }
